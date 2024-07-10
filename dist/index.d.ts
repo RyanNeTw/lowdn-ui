a10a21e5011074129c8fac7f45b10f5a55c01aac
LeRoiAdib <dev@romain-laurent.fr>
2024-07-10 23:22:37 +0200

    attempt at fixing exports

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import Image from './UI/Image';
+import type { IProps as FlagProps } from './UI/Image';
+import type { FlagStyle } from './types/types';
+export { Image, FlagProps, FlagStyle };
+//# sourceMappingURL=index.d.ts.map

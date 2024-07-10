43018ce95fe3c5f0cbb2d17e86b9f8a95d2394cd
LeRoiAdib <dev@romain-laurent.fr>
2024-07-10 17:48:27 +0200

    attempt at adding tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testMatch: [
+    '**/tests/**/*.+(ts|tsx|js|jsx)',
+    '**/tests/**/*.+(ts|tsx|js|jsx)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js|jsx)'
+  ]
+  // Configurez d'autres options de Jest ici selon vos besoins
+}
--- tests/example/Image.test.tsx
+++ tests/example/Image.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Image from '../../example/src/UI/Image'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Image country={'FR'} size={'24'} />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
